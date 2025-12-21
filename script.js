0182c8227e54acaa2046891f5a220ae345ccc0dd
SACHITH <kasasachith@gmail.com>
2025-12-21 12:52:25 +0530

    gets the required data on the user selected date

--- script.js
+++ script.js
@@ -1,85 +1,65 @@
-// var chart;
+google.charts.load('current', { packages: ['corechart'] });
 
-// document.getElementById("loadData").addEventListener("click", function () {
-//     var date = document.getElementById("selectedDate").value;
+const SPREADSHEET_ID = '1AdBjvpwcuAPetNtZXWR1nWwQTdbLCpslQ6xWbcPr5M0';
+const SHEET_PREFIX = 'EEE Block-1 Solar Data_Slave_1_';
 
-//     if (date === "") {
-//         alert("Please select a date");
-//         return;
-//     }
+function loadData() {
+  const dateValue = document.getElementById('datePicker').value;
 
-//     var sheetName = "EEE Block-1 Solar Data_Slave_1_" + date;
-//     var spreadsheetId = "1AdBjvpwcuAPetNtZXWR1nWwQTdbLCpslQ6xWbcPr5M0";
+  if (!dateValue) {
+    alert('Please select a date');
+    return;
+  }
 
-//     var url =
-//         "https://docs.google.com/spreadsheets/d/" +
-//         spreadsheetId +
-//         "/gviz/tq?tqx=out:json&sheet=" +
-//         encodeURIComponent(sheetName);
+  const sheetName = SHEET_PREFIX + dateValue;
 
-//     fetch(url)
-//         .then(res => res.text())
-//         .then(data => {
-//             var json = JSON.parse(data.substr(47).slice(0, -2));
-//             var rows = json.table.rows;
+  const queryString = `
+    SELECT A, B
+    WHERE A IS NOT NULL AND B IS NOT NULL
+  `;
 
-//             var timeLabels = [];
-//             var powerData = [];
+  const url =
+    `https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/gviz/tq?sheet=${encodeURIComponent(sheetName)}`;
 
-//             rows.forEach(row => {
-//                 if (row.c[0] && row.c[1]) {
-//                     var dateObj = new Date(row.c[0].v);
-//                     var time =
-//                         dateObj.getHours().toString().padStart(2, "0") + ":" +
-//                         dateObj.getMinutes().toString().padStart(2, "0") + ":" +
-//                         dateObj.getSeconds().toString().padStart(2, "0");
+  const query = new google.visualization.Query(url);
+  query.setQuery(queryString);
 
-//                     timeLabels.push(time);
-//                     powerData.push(row.c[1].v);
-//                 }
-//             });
+  query.send(function (response) {
+    if (response.isError()) {
+      document.getElementById('chart_div').innerHTML = '';
+      alert('No data found for selected date');
+      return;
+    }
 
-//             drawGraph(timeLabels, powerData);
-//         })
-//         .catch(err => console.log(err));
-// });
+    const data = response.getDataTable();
 
-// function drawGraph(timeLabels, powerData) {
-//     var ctx = document.getElementById("solarChart").getContext("2d");
+    if (!data || data.getNumberOfRows() === 0) {
+      document.getElementById('chart_div').innerHTML = '';
+      alert('No data found for selected date');
+      return;
+    }
 
-//     if (chart) {
-//         chart.destroy();
-//     }
+    drawChart(data);
+  });
+}
 
-//     chart = new Chart(ctx, {
-//         type: "line",
-//         data: {
-//             labels: timeLabels,
-//             datasets: [{
-//                 label: "Watts Generated",
-//                 data: powerData,
-//                 borderWidth: 2,
-//                 pointRadius: 0,
-//                 fill: false
-//             }]
-//         },
-//         options: {
-//             responsive: true,
-//             animation: false,
-//             scales: {
-//                 x: {
-//                     title: {
-//                         display: true,
-//                         text: "Time"
-//                     }
-//                 },
-//                 y: {
-//                     title: {
-//                         display: true,
-//                         text: "Watts"
-//                     }
-//                 }
-//             }
-//         }
-//     });
-// }
+function drawChart(data) {
+  const options = {
+    title: 'Solar Power Generation',
+    hAxis: {
+      title: 'Time',
+      format: 'HH:mm'
+    },
+    vAxis: {
+      title: 'Generated Power (Watts)',
+      minValue: 0
+    },
+    legend: 'none'
+  };
+
+  const chart = new google.visualization.LineChart(
+    document.getElementById('chart_div')
+  );
+
+  chart.draw(data, options);
+}
